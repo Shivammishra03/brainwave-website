6436f28a8317fb1203676db46eb2b18d1628fe19
Shivam Mishra <113275391+Shivammishra03@users.noreply.github.com>
2024-08-08 23:16:32 +0530

    "Added a new div element and wrapped existing h1 element in a fragment"

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <div>
+        I am going to be a famous 
+      </div>
+    </>
   )
 }
 
